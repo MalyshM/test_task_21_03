da9d0ba6038c7212f82a1f363cb16f477652e211
Малыш Михаил Сергеевич <>
2024-03-21 15:04:13 +0500

    make `handlers.py` and `validators.py`

--- db.py
+++ db.py
@@ -15,7 +15,6 @@ def fill_dict(group_type: str):
         "year": {"$year": "$dt"},
         "month": {"$month": "$dt"}
     }
-    # todo доделать
     date_from_parts = {
         "year": "$_id.year",
         "month": "$_id.month",
@@ -97,31 +96,11 @@ async def agg_data(group_type: str, date_from: datetime, date_to: datetime) -> s
         ]
         cursor = client.sampleDB.sample_collection.aggregate(max_pipeline_test)
         query = await cursor.to_list(length=None)
-        # print(query)
         result = res_scheme(date_from=date_from, date_to=date_to, group=group_type)
         for data, label in zip(query[0]['dataset'], query[0]['labels']):
             if label in result['labels']:
                 result['dataset'][result['labels'].index(label)] = data
-        print(result)
         client.close()
         return json.dumps(result)
     except Exception as e:
         print(e)
-
-# fill_dict(group_type="month")
-# fill_dict(group_type="day")
-# fill_dict(group_type="hour")
-# print("test case 1")
-# date_from = datetime(2022, 9, 1, 0, 0, 0, tzinfo=timezone.utc)
-# date_to = datetime(2022, 12, 31, 23, 59, 0, tzinfo=timezone.utc)
-# asyncio.run(agg_data(group_type="month", date_from=date_from, date_to=date_to))
-# print("\n\n\n\n")
-# print("test case 2")
-# date_from = datetime(2022, 10, 1, 0, 0, 0, tzinfo=timezone.utc)
-# date_to = datetime(2022, 11, 30, 23, 59, 0, tzinfo=timezone.utc)
-# asyncio.run(agg_data(group_type="day", date_from=date_from, date_to=date_to))
-# print("\n\n\n\n")
-# print("test case 3")
-# date_from = datetime(2022, 2, 1, 0, 0, 0, tzinfo=timezone.utc)
-# date_to = datetime(2022, 2, 2, 0, 0, 0, tzinfo=timezone.utc)
-# asyncio.run(agg_data(group_type="hour", date_from=date_from, date_to=date_to))
